067f78d27dece07befc56999d6e300cb85de739d
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-05-03 13:53:10 -0500

    Create edx_integration.js

---
+++ public/js/edx_integration.js
@@ -0,0 +1,31 @@
+(async () => {
+"use strict";
+    const path = window.location.pathname;
+    const parts = path.split("/");
+    const blockId = parts[parts.length - 1];
+    graspway_rating_init(blockId);
+
+    /**
+     * User Activity
+     */
+    function graspway_rating_init( block_id ) {
+    console.log('entra', block_id)
+    if (!block_id) { return ;}
+    const course_id = 'course-v1:' + block_id.match(/(^block\-v1\:)(.*)\+type\@(.*)(@)(.*)/i)[2];
+    const url = "/os-api/v1/courses/course/" + course_id + "/block/" + block_id + "/visit"
+
+    fetch(url)
+    .then(response => {
+        console.log('response1', response)
+        if (!response.ok) {
+            throw new Error('Network response was not success.');
+        }
+    })
+    .then(data => {
+        console.log('data', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+    }
+})();
