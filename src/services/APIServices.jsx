6bd991773e27db1481127997e07e9d40181e73c8
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-05-04 16:46:25 -0500

    Update APIServices.jsx

--- src/services/APIServices.jsx
+++ src/services/APIServices.jsx
@@ -1,16 +1,11 @@
 class APIService {
     visit = async (course_id, block_id) => {
         const url = "/os-api/v1/courses/course/" + course_id + "/block/" + block_id + "/visit"
-        console.log('url', url)
         fetch(url)
         .then(response => {
-            console.log('response1', response)
             if (!response.ok) {
                 throw new Error('Network response was not success.');
             }
-        })
-        .then(data => {
-            console.log('data', data);
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
