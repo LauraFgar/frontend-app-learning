0cf4ca5978098c511ce0edd67f2e564a916cb706
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-12-06 15:14:43 -0500

    fix: API GW URL

--- src/services/APIServices.jsx
+++ src/services/APIServices.jsx
@@ -1,7 +1,10 @@
+import {
+    getConfig,
+  } from '@edx/frontend-platform';
 class APIService {
     visit = async (course_id, block_id) => {
-        const URL = "/api/v1/courses/course/" + course_id + "/block/" + block_id + "/visit"
-        fetch( process.env.BASE_URL_EDX + URL)
+        const URL = "v1/courses/course/" + course_id + "/block/" + block_id + "/visit"
+        fetch( getConfig().API_GW_URL + URL)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not success.');
