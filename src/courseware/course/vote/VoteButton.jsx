ca62decb454b3cded625a161ae280da35bc18610
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-12-11 14:14:36 -0500

    feat: create VoteButton component

---
+++ src/courseware/course/vote/VoteButton.jsx
@@ -0,0 +1,42 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { IconButton } from '@edx/paragon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const VoteButton = ({
+  unitId,
+  courseId
+}) => {
+
+  const handleVoteClick = useCallback(async (nameEvent) => {
+    console.log('onClick', nameEvent, unitId, courseId)
+  }, [unitId, courseId]);
+
+  return (
+    <div>
+      <IconButton 
+        key="ilike" 
+        variant="success" 
+        alt="Like" 
+        iconAs={<FontAwesomeIcon icon="fa-solid fa-thumbs-up" />} 
+        onClick={handleVoteClick("like")} 
+        className="mr-2" 
+      />
+      <IconButton 
+        key="inlike" 
+        variant="danger" 
+        alt="Like" 
+        iconAs={<FontAwesomeIcon icon="fa-solid fa-thumbs-down" />} 
+        onClick={handleVoteClick("inlike")} 
+        className="mr-2" 
+      />
+    </div>
+  );
+};
+
+VoteButton.propTypes = {
+  unitId: PropTypes.string.isRequired,
+  courseId: PropTypes.string.isRequired,
+};
+
+export default VoteButton;
