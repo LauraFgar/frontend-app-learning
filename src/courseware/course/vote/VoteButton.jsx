166b3b952ebfa1c2edcb2a0d530452c1b6bb913d
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-12-12 15:02:28 -0500

    Update VoteButton.jsx

--- src/courseware/course/vote/VoteButton.jsx
+++ src/courseware/course/vote/VoteButton.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { IconButton } from '@edx/paragon';
 import { ThumbUpOutline, ThumbDownOffAlt } from '@edx/paragon/icons';
