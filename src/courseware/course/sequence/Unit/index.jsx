ae3647dbd8263d00e6fa884c2de70375c0e83b4a
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2023-12-11 21:21:15 -0500

    Update index.jsx

--- src/courseware/course/sequence/Unit/index.jsx
+++ src/courseware/course/sequence/Unit/index.jsx
@@ -7,6 +7,7 @@ import { useIntl } from '@edx/frontend-platform/i18n';
 import { useModel } from '../../../../generic/model-store';
 
 import BookmarkButton from '../../bookmark/BookmarkButton';
+import VoteButton from '../../vote/VoteButton';
 import messages from '../messages';
 import ContentIFrame from './ContentIFrame';
 import UnitSuspense from './UnitSuspense';
@@ -45,6 +46,10 @@ const Unit = ({
         unitId={unit.id}
         isBookmarked={unit.bookmarked}
         isProcessing={isProcessing}
+      />
+      <VoteButton
+        courseId={courseId}
+        unitId={unit.id}
       />
       <UnitSuspense {...{ courseId, id }} />
       <ContentIFrame
